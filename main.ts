e64c81d4e25953a01cced2298e3c055f18c128c7
faruqismael <farusmith5@gmail.com>
2021-03-20 21:50:03 +0300

    finished all

--- main.ts
+++ main.ts
@@ -14,34 +14,46 @@ class NumberSystem {
 
     }
 
-    // binary to decimal
-    binaryToDecimal(binary: number): number {
-        let binaryString = binary.toString();
+    // for all 
+    toDecimal(num: number, base: number): number {
+        let binaryString = num.toString();
         let index = binaryString.length - 1;
 
         for (let i = 0; i < binaryString.length; i++) {
-            this.result += Number(binaryString[i]) * (2 ** index)
+            this.result += Number(binaryString[i]) * (base ** index)
             index--;
         }
         return this.result
     }
 
     // decimal to binary
-    decimalToBinary(decimal: number): number {
+    fromDecimal(decimal: number, base: number): number {
         let storeRemainder: string = '';
 
         while (decimal != 0) {
-            storeRemainder += decimal % 2;
-            decimal = Math.floor(decimal / 2);
+            storeRemainder += decimal % base;
+            decimal = Math.floor(decimal / base);
         }
-
         return Number(this.reverse(storeRemainder))
     }
+
+    // simple shortcuts
+
+    // from binary
+    fromBinary(binary: number, base: number): number {
+        let decimal = this.toDecimal(binary, 2)
+        return this.fromDecimal(decimal, base)
+        // return 1
+    }
+
+    // to binary
+
+    toBinary(number: number, base: number) {
+        let decimal = this.toDecimal(number, base)
+        return this.fromDecimal(decimal, 2)
+    }
+
 }
-
-let one = new NumberSystem();
-
-console.log(one.binaryToDecimal(110))
 
 
 export default NumberSystem;
