67013cfade3041a8f3d47c8c74876af590421a94
faruqismael <farusmith5@gmail.com>
2021-03-21 11:17:31 +0300

    using: type base

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+type base = 2 | 8 | 10 | 16;
+
 class NumberSystem {
 
     result = 0;
@@ -15,7 +17,7 @@ class NumberSystem {
     }
 
     // from any number system to decimal
-    toDecimal(num: number, base: number): number {
+    toDecimal(num: number, base: base): number {
         let binaryString = num.toString();
         let index = binaryString.length - 1;
 
@@ -27,7 +29,7 @@ class NumberSystem {
     }
 
     // decimal to any number system
-    fromDecimal(decimal: number, base: number): number {
+    fromDecimal(decimal: number, base: base): number {
         let storeRemainder: string = '';
 
         while (decimal != 0) {
@@ -39,14 +41,14 @@ class NumberSystem {
 
 
     // from binary to any number
-    fromBinary(binary: number, base: number): number {
+    fromBinary(binary: number, base: base): number {
         let decimal = this.toDecimal(binary, 2)
         return this.fromDecimal(decimal, base)
     }
 
     // any number to binary
 
-    toBinary(number: number, base: number) {
+    toBinary(number: number, base: base) {
         let decimal = this.toDecimal(number, base)
         return this.fromDecimal(decimal, 2)
     }
@@ -55,3 +57,5 @@ class NumberSystem {
 
 
 export default NumberSystem;
+let ns = new NumberSystem();
+
